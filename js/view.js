c4ad0100c24d8d8bde77d10f666ed14bd429a349
Ke You Chen <gemini81612@gmail.com>
2022-04-20 14:56:32 +0800

    [create]: create file view.js

---
+++ js/view.js
@@ -0,0 +1,16 @@
+function render(weatherData){
+  ////天氣資訊呈現
+  document.getElementById("weather_condition").innerHTML = weatherData.regionWeather.Wx;
+  document.getElementById("weather_temparature").innerHTML = weatherData.regionWeather.MaxT + "/" + weatherData.regionWeather.MinT;
+  document.getElementById("weather_rain").innerHTML = weatherData.regionWeather.PoP + "%";
+  document.getElementById("weather_comfort").innerHTML = weatherData.regionWeather.comfort;
+  ////地區
+  document.getElementById("region").innerHTML = weatherData.regionWeather.region;
+  ////日期
+  document.getElementById("date_month").innerHTML = weatherData.regionWeather.date.momth;
+  document.getElementById("date_date").innerHTML = weatherData.regionWeather.date.date;
+  ////good or bad
+  document.getElementById("good_todo").innerHTML = weatherData.GoB.good;
+  document.getElementById("bad_todo").innerHTML = weatherData.GoB.bad;
+}
+
